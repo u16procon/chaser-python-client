6b25fe9f9e1affd70baca5d5c6cffc0d46d800f3
Daichi Onodera <onodes@gmail.com>
2022-07-03 23:41:57 +0900

    サンプルコードをサーチのみの動作に変更

--- sample.py
+++ sample.py
@@ -6,33 +6,22 @@ def main():
     client.connect()
 
     while True:
+        # 制御コードとマップ情報を得る
         control_code, map_info = client.receive()
+        # 制御コードが0の場合ループを抜ける
         if control_code == 0:
             break
 
+        # 準備ができたらget_ready()を行う
         control_code, map_info = client.get_ready()
+        # 自身の左をサーチする
         control_code, map_info = client.search_left()
+        # map情報をコンソールに表示する
         print(map_info)
+        # ターンを終了する
         client.turn_end()
 
-        control_code, map_info = client.receive()
-        control_code, map_info = client.get_ready()
-        print(map_info)
-        if map_info[7] != 2:
-            control_code, map_info = client.walk_down()
-        else:
-            control_code, map_info = client.put_up()
-        client.turn_end()
-
-        control_code, map_info = client.receive()
-        control_code, map_info = client.get_ready()
-        control_code, map_info = client.look_up()
-        client.turn_end()
-
-        control_code, map_info = client.receive()
-        control_code, map_info = client.get_ready()
-        control_code, map_info = client.put_right()
-        client.turn_end()
+    client.close()
 
 
 if __name__ == "__main__":
