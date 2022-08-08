a61e821f5d0a01ce5a7456e1b6dc18a836f6d672
Daichi Onodera <onodes@gmail.com>
2022-08-08 22:40:47 +0900

    ChaserSimpleClientに指定する引数のコメントを追記

--- sample_SimpleClient.py
+++ sample_SimpleClient.py
@@ -2,9 +2,11 @@ from client.ChaserSimpleClient import ChaserSimpleClient
 
 
 def main():
+    # ChaserSimpleClient("CHaserサーバが動いているマシンのIPアドレス", "CHaserサーバのポート番号", "チームや自身の名前")
     client = ChaserSimpleClient("192.168.xx.x", 2009, "test")
 
     while True:
+        # マップ情報を記載する空listを作成
         map_info = []
 
         # 準備ができたらget_ready()を行う
